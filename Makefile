e9f1455d7b70c433f2349412d8b5786123d54c2a
boyutov <boytov97@gmail.com>
2022-11-08 17:00:23 +0600

    -feat: Base job is done. Making readme document is in progress

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
-run_kafka_server:
+# - Run all containers
+run_containers:
 	docker-compose up
+# - Connect to php container to run consumer or producer
+connect_php_container:
+	docker exec -it php-rdkafka bash
--- public/consumer.php
+++ public/consumer.php
@@ -2,7 +2,7 @@
 
 $conf = new \RdKafka\Conf();
 $conf->set('bootstrap.servers', 'kafka:9092');
-$conf->set('enable.auto.commit', 'false');
+$conf->set('group.id', 'my_consumer_group');
 
 $consumer = new \RdKafka\Consumer($conf);
 $consumer->setLogLevel(LOG_DEBUG);
@@ -12,7 +12,12 @@ $topic = $consumer->newTopic("php_rdkafka");
 
 $partition = 0;
 
-$topic->consumeStart($partition, RD_KAFKA_OFFSET_BEGINNING);
+/*
+ * RD_KAFKA_OFFSET_STORED
+ *
+ * RD_KAFKA_OFFSET_BEGINNING
+ */
+$topic->consumeStart($partition, RD_KAFKA_OFFSET_STORED);
 
 echo "consumer started" . PHP_EOL;
 
@@ -22,11 +27,9 @@ while (true) {
     $tick++;
     $msg = $topic->consume($partition, 1000);
 
-    var_dump($tick);
+    echo "tick: {$tick}" . PHP_EOL;
 
     if ($msg->payload) {
-        echo $msg->payload, "\n";
-
-        $topic->offsetStore($partition, $msg->offset);
+        echo $msg->payload . PHP_EOL;
     }
 }
